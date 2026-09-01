136b453549308a43767713cd639962ef7e029e1a
Anuj Dubey <anujd2408@gmail.com>
2026-09-01 13:58:22 +0530

    added deserialize_history function

--- memory.py
+++ memory.py
@@ -1,4 +1,5 @@
-
+from google.genai import types
+import json
 
 def serialize_history(history):
     serialized = []
@@ -22,3 +23,25 @@ def serialize_history(history):
         serialized.append(entry)
     return serialized
 
+def deserialize_history(serialized):
+    history = []
+    for entry in serialized:
+        parts =[]
+        for part_data in entry["parts"]:
+            if part_data["type"] == "text":
+                parts.append(types.Part.from_text(text=part_data["text"]))
+            elif part_data["type"] == "function_call":
+                parts.append(types.Part.from_function_call(name=part_data["name"], args=part_data["args"]))
+            elif part_data["type"] == "function_response":
+                parts.append(types.Part.from_function_response(name=part_data["name"], response=part_data["response"]))
+
+        history.append(types.Content(role=entry["role"], parts=parts))
+    return history
+
+def save_history(history, filepath="history.json"):
+    with open(filepath, "w") as f:
+        json.dump(serialize_history(history), f, indent=2)
+
+def load_history(filepath="history.json"):
+    with open(filepath, "r") as f:
+        return deserialize_history(json.load(f));
